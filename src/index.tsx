9de6f18d06b40e7ec368c3dafd15c1c6890bc8a0
Ivan <vanna7417@gmail.com>
2023-12-17 16:18:13 +0100

    created compounent

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+
+function App() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+                maxime laudantium consequuntur amet eaque molestiae itaque
+                beatae aspernatur, repellat placeat. Ea ipsam saepe libero
+                quaerat tempore delectus architecto. Sint, illum.
+            </p>
+        </div>
+    )
+}
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
